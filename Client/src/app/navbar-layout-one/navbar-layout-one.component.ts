0a01cac7b8d64a725a85e995899b120351c3f7ce
Heine breimo <heine.breimo@tietoevry.com>
2026-01-26 14:03:29 +0100

    wip part 2

--- Client/src/app/navbar-layout-one/navbar-layout-one.component.ts
+++ Client/src/app/navbar-layout-one/navbar-layout-one.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar-layout-one',
   templateUrl: './navbar-layout-one.component.html',
-  styleUrl: './navbar-layout-one.component.css'
+  styleUrls: ['./navbar-layout-one.component.css',
+    '../navbar/navbar.component.css'
+  ]
 })
 export class NavbarLayoutOneComponent {
 
